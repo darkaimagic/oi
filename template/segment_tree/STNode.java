08ccb00215e1b2e5f83e61357f0532e16312d2c8
darkaimagic <darkaimagic@gmail.com>
2020-10-14 21:57:38 -0700

    update segment_tree to range inc and range sum

--- template/segment_tree/STNode.java
+++ template/segment_tree/STNode.java
@@ -1,37 +1,54 @@
 class STNode {
-    int from, to, sum, cnt;
-    Node left, right;
-    Node(int from, int to) {
+    int from, to, mid;
+    long incVal, sumVal;
+    STNode left, right;
+    STNode(int from, int to) {
         this.from = from;
         this.to = to;
-        sum = 0;
-        cnt = 0;
+        mid = from + (to - from) / 2;
+        incVal = 0;
+        sumVal = 0;
         left = null;
         right = null;
     }
-    void add(int x) {
-        sum += x;
-        cnt++;
-        if (from < to) {
-            int mid = from + (to - from) / 2;
-            if (x <= mid) {
-                if (left == null)
-                    left = new Node(from, mid);
-                left.add(x);
-            } else {
-                if (right == null)
-                    right = new Node(mid + 1, to);
-                right.add(x);
-            }
+    void inc(int p, int q, long val) {
+        sumVal += val * (q - p + 1);
+        if (p == from && q == to) {
+            incVal += val;
+            return;
+        }
+        if (p <= mid && left == null)
+            left = new STNode(from, mid);
+        if (q > mid && right == null)
+            right = new STNode(mid + 1, to);
+        if (q <= mid)
+            left.inc(p, q, val);
+        else if (p > mid)
+            right.inc(p, q, val);
+        else {
+            left.inc(p, mid, val);
+            right.inc(mid + 1, q, val);
         }
     }
-    int get(int idx) {
-        if (from == to)
-            return from;
-        if (left != null && idx < left.cnt)
-            return left.get(idx);
-        if (left != null)
-            idx -= left.cnt;
-        return right.get(idx);
+    long sum(int p, int q) {
+        if (p == from && q == to)
+            return sumVal;
+        pushDown();
+        if (q <= mid)
+            return left.sum(p, q);
+        else if (p > mid)
+            return right.sum(p, q);
+        return left.sum(p, mid) + right.sum(mid + 1, q);
+    }
+    void pushDown() {
+        if (left == null)
+            left = new STNode(from, mid);
+        if (right == null)
+            right = new STNode(mid + 1, to);
+        left.incVal += incVal;
+        right.incVal += incVal;
+        left.sumVal += incVal * (left.to - left.from + 1);
+        right.sumVal += incVal * (right.to - right.from + 1);
+        incVal = 0;
     }
 }
